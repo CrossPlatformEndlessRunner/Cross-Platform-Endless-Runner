964da7a4772f98e7a5081127999ae0af4f6f0ad5
cufffs <callum.buerckner@hotmail.com>
2015-09-21 17:20:23 +1000

    Swiping

---
+++ EndlessRunnerAndroid/Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//attach to camera
+
+public class CameraTracking : MonoBehaviour {
+
+	//target
+	[SerializeField]
+	public GameObject m_targ;
+
+	void Start () {
+	}
+	
+	void LateUpdate () {
+		//follow x, ignore y, z
+		transform.position = new Vector3(m_targ.transform.position.x, transform.position.y, transform.position.z);
+	}
+}
