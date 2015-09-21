537e97dc04210883a406bf24bab5d493e0079c27
cufffs <callum.buerckner@hotmail.com>
2015-09-21 18:19:21 +1000

    test_scene

--- EndlessRunnerAndroid/Assets/Scripts/InputController.cs
+++ EndlessRunnerAndroid/Assets/Scripts/InputController.cs
@@ -23,7 +23,9 @@ public class InputController : MonoBehaviour {
 
 	//jump height that player will jump
 	public float m_jump_height;
-	//time before boost can activate again
+	//placeholder
+	public float m_boost_speed;
+	//time before boost can activate again (seconds)
 	public float m_time_between_boosts;
 	//distance finger/mouse must be dragged before activation
 	public float m_swipe_distance;
@@ -36,6 +38,7 @@ public class InputController : MonoBehaviour {
 	*/
 
 
+	//keep track of boost cooldown remaining time
 	private float m_boost_cd_time;
 
 	//initial position of mousedown/touch
@@ -47,6 +50,7 @@ public class InputController : MonoBehaviour {
 	private bool m_touch_supported = false;
 
 	void Start () {
+		//detect if touch or mouse
 		if (SystemInfo.deviceType == DeviceType.Handheld)
 			m_touch_supported = true;
 		m_swipe_direction = new Swipe_Direction ();
@@ -66,10 +70,11 @@ public class InputController : MonoBehaviour {
 		if (m_swipe_direction.up && player.velocity.y == 0.0f) {
 			player.AddForce (new Vector2 (0, m_jump_height));
 		} else if (m_swipe_direction.down && player.velocity.y > 0.0f) {
+			//swipe down: stop moving up and begin to move down
 			player.velocity = new Vector2(player.velocity.x, 0.0f);
 		} else if (m_swipe_direction.right && m_boost_cd_time <= 0.0f) {
 			//do something boosty
-			player.AddForce(new Vector2(50, 0));
+			player.AddForce(new Vector2(m_boost_speed, 0));
 			m_boost_cd_time = m_time_between_boosts;
 		}
 
@@ -91,7 +96,8 @@ public class InputController : MonoBehaviour {
 			//only do stuff after swipe completed
 			if(touch.phase == TouchPhase.Ended) {
 				Vector2 end_pos = new Vector2(touch.position.x, touch.position.y);
-				if(Vector2.Distance(m_orig_pos, end_pos) > m_swipe_distance){
+				//if distance > minimum distance and is a swipe and not holding down
+				if(Vector2.Distance(m_orig_pos, end_pos) > m_swipe_distance && touch.deltaPosition.magnitude != 0.0f){
 					//create new swipe vector, normalise it and then calc direction of it
 					calc_swipe_direction((new Vector2(touch.position.x - m_orig_pos.x, touch.position.y - m_orig_pos.y)).normalized);
 				}
@@ -110,7 +116,8 @@ public class InputController : MonoBehaviour {
 		if (Input.GetMouseButtonUp (0)) {
 			Vector2 end_mouse_pos = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
 			//only do stuff if the swipe distance > minimum swipe distance
-			if (Vector2.Distance (m_orig_pos, end_mouse_pos) > m_swipe_distance) {
+			if (Vector2.Distance (m_orig_pos, end_mouse_pos) > m_swipe_distance && 
+			    (Input.GetAxis ("Mouse X") != 0.0f || Input.GetAxis ("Mouse Y")!= 0.0f)) {
 				//create new swipe vector, normalise it and then calc direction of it
 				calc_swipe_direction((new Vector2 (end_mouse_pos.x - m_orig_pos.x, end_mouse_pos.y - m_orig_pos.y)).normalized);
 			}
@@ -120,21 +127,21 @@ public class InputController : MonoBehaviour {
 	private void calc_swipe_direction(Vector2 swipe)
 	{
 		//get mousemovement, so cannot hold mouse, must be moving mouse in order to have response
-		float mousex = Input.GetAxis ("Mouse X"), mousey = Input.GetAxis ("Mouse Y");
+		//float mousex = Input.GetAxis ("Mouse X"), mousey = Input.GetAxis ("Mouse Y");
 		//swipe up
-		if (swipe.y > 0 && swipe.x > -m_swipe_band && swipe.x < m_swipe_band && mousey>0.0f) {
+		if (swipe.y > 0 && swipe.x > -m_swipe_band && swipe.x < m_swipe_band /*&& mousey>0.0f*/) {
 			m_swipe_direction.up = true;
 		}
 		//swipe down
-		else if (swipe.y < 0 && swipe.x > -m_swipe_band && swipe.x < m_swipe_band && mousey<0.0f) {
+		else if (swipe.y < 0 && swipe.x > -m_swipe_band && swipe.x < m_swipe_band /*&& mousey<0.0f*/) {
 			m_swipe_direction.down = true;
 		}		
 		//swipe left
-		if (swipe.x < 0 && swipe.y > -m_swipe_band && swipe.y < m_swipe_band && mousex<0.0f) {
+		if (swipe.x < 0 && swipe.y > -m_swipe_band && swipe.y < m_swipe_band /*&& mousex<0.0f*/) {
 			m_swipe_direction.left = true;
 		}
 		//swipe right
-		else if (swipe.x > 0 && swipe.y > -m_swipe_band && swipe.y < m_swipe_band && mousex>0.0f) {
+		else if (swipe.x > 0 && swipe.y > -m_swipe_band && swipe.y < m_swipe_band /*&& mousex>0.0f*/) {
 			m_swipe_direction.right = true;
 		}
 	}
