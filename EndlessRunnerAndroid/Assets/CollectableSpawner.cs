665f58e2d61d19476569ae2991e4d6c2bfe8ec9f
cufffs <callum.buerckner@hotmail.com>
2015-10-27 18:46:15 +1100

    Collectables up and somewhat running
    needs to add score somewhere

---
+++ EndlessRunnerAndroid/Assets/CollectableSpawner.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//sorry
+
+public class CollectableSpawner : MonoBehaviour {
+
+    List<GameObject> _obj_pool;
+
+    //prefab stuff
+    static public int _num_prefabs = 5;
+    public GameObject[] _collectables_prefabs = new GameObject[_num_prefabs];
+
+    //this should be same speed as obstacles
+    public int global_speed = 2;
+
+    private int _pool_size = 0;
+
+    public int _spawn_time = 5;
+    private float timer = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+        _obj_pool = new List<GameObject>();
+        _pool_size = _collectables_prefabs.Length;
+        //cpy paste from yours :P
+        for (int i = 0; i < _pool_size; ++i)
+        {
+            GameObject obj = (GameObject)Instantiate(_collectables_prefabs[i]);
+            obj.SetActive(false);
+            obj.layer = LayerMask.NameToLayer("Collectables");
+            _obj_pool.Add(obj);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer > _spawn_time)
+        {
+            spawn_object();
+            timer = 0.0f;
+        }
+
+        //collectables move themselves
+
+        //for (int i = 0; i < _pool_size; ++i)
+        //{
+        //    if (_obj_pool[i].activeInHierarchy)
+        //    {
+        //        var pos = _obj_pool[i].transform.position;
+        //        //each could have unique speed
+        //        pos.x -= global_speed * Time.deltaTime;
+        //        _obj_pool[i].transform.position = pos;
+        //    }
+        //}
+	}
+
+    //just morphed your code to work with mine :P
+    void spawn_object()
+    {
+        int cur_obj = 0;
+        for (; cur_obj < _pool_size; ++cur_obj)
+        {
+            if (!_obj_pool[cur_obj].activeInHierarchy)
+                break;
+        }
+        //may be buggy
+        if (cur_obj == _pool_size)
+        {
+            Debug.Log("No valid collectables to spawn");
+            return;
+        }
+
+        GameObject to_spawn = _obj_pool[cur_obj];
+        to_spawn.transform.position = gameObject.transform.position;
+        to_spawn.transform.rotation = gameObject.transform.rotation;
+        to_spawn.SetActive(true);
+    }
+}
--- EndlessRunnerAndroid/Assets/Collectables.cs
+++ EndlessRunnerAndroid/Assets/Collectables.cs
@@ -3,18 +3,24 @@ using System.Collections;
 
 public class Collectables : MonoBehaviour {
 
-    public int _points_value;
+    public int _points_value = 10;
+    public float _speed = 0.5f;
 
 	void Start () {
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Obstacles"), LayerMask.NameToLayer("Collectables")); //layers 9, 10
 	}
 	
 	void Update () {
-	
+        var pos = transform.position;
+        pos.x += -_speed*Time.deltaTime;
+        transform.position = pos;
 	}
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        //print("collision");
+        print("collecting");
+        gameObject.SetActive(false);
+
+        //needs to add points somewhere
     }
 }
