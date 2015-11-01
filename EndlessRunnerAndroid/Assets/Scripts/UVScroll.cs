572117650b199fc0d3084a74ae7f5990c96b1b32
Tronobro <reginald.ashman@students.aie.edu.au>
2015-11-02 00:21:35 +1030

    Added scrolling background and animated tumbleweed

---
+++ EndlessRunnerAndroid/Assets/Scripts/UVScroll.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UVScroll : MonoBehaviour
+{
+    public Vector2 scrollSpeed;
+    private Renderer rendererOne = null;
+    // Use this for initialization
+    void Start()
+    {
+        rendererOne = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        rendererOne.material.mainTextureOffset = scrollSpeed * Time.time;
+    }
+}
